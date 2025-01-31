2296f1cc6eebf7efe3b683baeb98cfa4ac09fd09
Natalia Terehova <natallia.puzikova@gmail.com>
2025-01-31 15:34:18 +0300

    Adding initial files to project

---
+++ test_main_page.py
@@ -0,0 +1,16 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import math
+
+
+def test_guest_can_go_to_login_page(browser):
+    browser.implicitly_wait(5)
+    link = "http://selenium1py.pythonanywhere.com/"
+    browser.get(link)
+    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
+    login_link.click()
+        
