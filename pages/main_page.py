6b6e14713977713109f86fc51098f56fffe9dadf
Natalia Terehova <natallia.puzikova@gmail.com>
2025-02-03 18:56:15 +0300

    Changed selectors to locators

--- pages/main_page.py
+++ pages/main_page.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from .base_page import BasePage
+from .locators import MainPageLocators
 import time
 import math
 
@@ -11,8 +12,8 @@ import math
 class MainPage(BasePage):
 
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
 
     def should_be_login_link(self):
-        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
