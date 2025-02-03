f5f97972de3db73ea0286d90febfbc95c2c5439f
Natalia Terehova <natallia.puzikova@gmail.com>
2025-02-03 18:57:37 +0300

    Added test for login page

--- test_main_page.py
+++ test_main_page.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 import time
 import math
 
@@ -24,3 +25,21 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+def test_login_url(browser):
+    link = "https://selenium1py.pythonanywhere.com/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_url()
+
+def test_login_form(browser):
+    link = "https://selenium1py.pythonanywhere.com/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_form()
+    
+def test_register_form(browser):
+    link = "https://selenium1py.pythonanywhere.com/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_register_form()
