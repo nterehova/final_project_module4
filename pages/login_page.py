cd43be8b7b16e33ff73c0b768e79f1891fdab9fb
Natalia Terehova <natallia.puzikova@gmail.com>
2025-02-03 18:52:22 +0300

    Added new file login page with class LoginPage

---
+++ pages/login_page.py
@@ -0,0 +1,29 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.remote.webdriver import WebDriver
+from .locators import LoginPageLocators
+from .base_page import BasePage
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        current_url = self.browser.current_url
+        url_check = current_url.find("login")
+        print(url_check)
+        assert url_check != -1, "Url is not correct"
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.login_form), "Login form is not present"
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.register_form), "Register form is not present"
+
+
