adc8727c0d171c891ace262ab972523cd66b7eb7
Natalia Terehova <natallia.puzikova@gmail.com>
2025-02-04 16:07:49 +0300

    Updated test to initialize LoginPage inside the test

--- test_main_page.py
+++ test_main_page.py
@@ -9,37 +9,35 @@ import time
 import math
 
 
-#def go_to_login_page(browser):
-#    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-#    login_link.click()
-
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
         
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+#def test_guest_should_see_login_link(browser):
+#    link = "http://selenium1py.pythonanywhere.com/"
+#    page = MainPage(browser, link)
+#    page.open()
+#    page.should_be_login_link()
 
-def test_login_url(browser):
-    link = "https://selenium1py.pythonanywhere.com/accounts/login/"
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_login_url()
+#def test_login_url(browser):
+#    link = "https://selenium1py.pythonanywhere.com/accounts/login/"
+#    page = LoginPage(browser, link)
+#    page.open()
+#    page.should_be_login_url()
 
-def test_login_form(browser):
-    link = "https://selenium1py.pythonanywhere.com/accounts/login/"
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_login_form()
+#def test_login_form(browser):
+#    link = "https://selenium1py.pythonanywhere.com/accounts/login/"
+#    page = LoginPage(browser, link)
+#    page.open()
+#    page.should_be_login_form()
     
-def test_register_form(browser):
-    link = "https://selenium1py.pythonanywhere.com/accounts/login/"
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_register_form()
+#def test_register_form(browser):
+#    link = "https://selenium1py.pythonanywhere.com/accounts/login/"
+#    page = LoginPage(browser, link)
+#    page.open()
+#    page.should_be_register_form()
