858862a202edb7377d1163bfb760077af4e9b42a
Natalia Terehova <natallia.puzikova@gmail.com>
2025-02-04 16:06:28 +0300

    Updated go to login page method

--- pages/main_page.py
+++ pages/main_page.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from .base_page import BasePage
 from .locators import MainPageLocators
+from .login_page import LoginPage
 import time
 import math
 
@@ -12,8 +13,8 @@ import math
 class MainPage(BasePage):
 
     def go_to_login_page(self):
-        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
-        login_link.click()
+        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        link.click()
 
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
