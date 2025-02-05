e408e551950550a7d472f1af1132ccfd99b44a87
Natalia Terehova <natallia.puzikova@gmail.com>
2025-02-05 19:52:17 +0300

    Added locators for Product page

--- pages/locators.py
+++ pages/locators.py
@@ -11,3 +11,19 @@ class MainPageLocators():
 class LoginPageLocators():
     login_form = (By.CSS_SELECTOR, "form#login_form")
     register_form = (By.CSS_SELECTOR, "form#register_form")
+
+class ProductPageLocators():
+    add_to_cart_button = (By.CSS_SELECTOR, "button.btn-add-to-basket")
+    
+    #изначальные вебэлементы с атрибутами продукта на странице
+    product_name = (By.XPATH, "//*[@id='content_inner']/article/div[1]/div[2]/h1")
+    product_price = (By.CSS_SELECTOR, "p.price_color")
+    
+    # название и цена продукта в сообщениях
+    message_product_added_to_cart =(By.XPATH, "//*[@id='messages']/div[1]/div")
+    product_name_in_success_message = (By.XPATH, "//*[@id='messages']/div[1]/div/strong")
+    message_total_cart_price =(By.XPATH, "//*[@id='messages']/div[3]/div/p[1]")
+    cart_price_in_message = (By.XPATH, "//*[@id='messages']/div[3]/div/p[1]/strong")
+    #корзина в хэдере
+    mini_cart_in_header = (By.XPATH, "//*[@id='default']/header/div[1]/div/div[2]")
+
