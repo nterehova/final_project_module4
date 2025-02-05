66772a6b925c081cb656d471f2343f51fd0710a7
Natalia Terehova <natallia.puzikova@gmail.com>
2025-02-05 19:51:20 +0300

    Added product page with Product class and added tests in test_product_page

---
+++ pages/product_page.py
@@ -0,0 +1,41 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+
+    def add_product_to_cart(self):
+        button = self.browser.find_element(*ProductPageLocators.add_to_cart_button)
+        button.click()
+        self.solve_quiz_and_get_code()
+
+    def should_be_message_about_added_product(self):
+        assert self.is_element_present(*ProductPageLocators.message_product_added_to_cart), "Product is not added"
+    
+    def return_product_name(self):
+        product_name = self.browser.find_element(*ProductPageLocators.product_name)
+        return product_name.text
+        
+    def return_product_price(self):
+        product_price = self.browser.find_element(*ProductPageLocators.product_price)
+        return product_price.text
+        
+    def should_be_correct_product_added(self):
+        #product_name = self.browser.find_element(*ProductPageLocators.product_name)
+        product_name = self.return_product_name()
+        product_name_in_message = self.browser.find_element(*ProductPageLocators.product_name_in_success_message)
+        assert product_name == product_name_in_message.text, "Wrong product added to cart"
+        
+    def should_be_message_with_cart_price(self):
+        assert self.is_element_present(*ProductPageLocators.message_total_cart_price), "There is no success message with price"
+    
+    def should_be_correct_price_in_message(self):
+        #product_price = self.browser.find_element(*ProductPageLocators.product_price)
+        product_price = self.return_product_price()
+        cart_price_in_message = self.browser.find_element(*ProductPageLocators.cart_price_in_message)
+        assert product_price == cart_price_in_message.text, "Cart price is correct"
---
+++ test_product_page.py
@@ -0,0 +1,22 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from .pages.main_page import MainPage
+from .pages.product_page import ProductPage
+import time
+import math
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0", "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1", "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2", "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3", "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4", "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5", "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6", pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail), "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8", "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_guest_can_add_product_to_basket(browser, link):
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.add_product_to_cart()
+    browser.implicitly_wait(3)
+    product_page.should_be_message_about_added_product()
+    product_page.should_be_correct_product_added()
+    product_page.should_be_message_with_cart_price()
+    product_page.should_be_correct_price_in_message()
