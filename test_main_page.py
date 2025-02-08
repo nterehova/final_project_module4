ea020d0c0b9ea52ecb1e480ca167ca14cebd8823
Natalia Terehova <natallia.puzikova@gmail.com>
2025-02-09 00:24:49 +0300

    Added test to check empty cart

--- test_main_page.py
+++ test_main_page.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
+from .pages.basket_page import BasketPage
 import time
 import math
 
@@ -17,6 +18,14 @@ def test_guest_can_go_to_login_page(browser):
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
 
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = BasketPage(browser, link)
+    page.open()
+    page.should_go_to_cart()
+    page.is_no_product_in_cart()
+    page.should_be_empty_cart_message()
+    
         
 #def test_guest_should_see_login_link(browser):
 #    link = "http://selenium1py.pythonanywhere.com/"
