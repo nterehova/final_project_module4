959f4eba576080d84541a1387d5ff0ad99290df1
Natalia Terehova <natallia.puzikova@gmail.com>
2025-02-09 00:24:05 +0300

    Removed extra locators

--- pages/main_page.py
+++ pages/main_page.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from .base_page import BasePage
-from .locators import MainPageLocators
+#from .locators import MainPageLocators
 from .login_page import LoginPage
 import time
 import math
