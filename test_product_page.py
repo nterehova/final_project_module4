7be362fb2877d0dd3da5df2d0042f1754a67cc18
Natalia Terehova <natallia.puzikova@gmail.com>
2025-02-10 02:17:48 +0300

    Added test class TestUserAddToBasketFromProductPage with setup fixture + minor edits

--- test_product_page.py
+++ test_product_page.py
@@ -5,8 +5,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from .pages.product_page import ProductPage
 from .pages.basket_page import BasketPage
+from .pages.login_page import LoginPage
 import time
-import math
 
 
 def test_guest_should_see_login_link_on_product_page(browser):
@@ -22,9 +22,11 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.go_to_login_page()
     page.should_be_login_url()
 
+
 @pytest.mark.skip
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0", "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1", "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2", "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3", "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4", "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5", "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6", pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail), "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8", "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_guest_can_add_product_to_basket(link, browser):
+    #link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.add_product_to_cart()
@@ -63,3 +65,33 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     product_page.should_go_to_cart()
     product_page.is_no_product_in_cart()
     product_page.should_be_empty_cart_message()
+
+class TestUserAddToBasketFromProductPage():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+        page = LoginPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        email = str(time.time()) + "@test.com"
+        page.register_new_user(email, "999sel11m")
+        page.should_be_authorized_user
+
+        
+    def test_user_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+        product_page = ProductPage(browser, link)
+        product_page.open()
+        #нужно убирать solve_quiz в product_page чтобы не падало
+        product_page.add_product_to_cart()
+        browser.implicitly_wait(3)
+        product_page.should_be_message_about_added_product()
+        product_page.should_be_correct_product_added()
+        product_page.should_be_message_with_cart_price()
+        product_page.should_be_correct_price_in_message()
+
+    def test_user_cant_see_success_message(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+        product_page = ProductPage(browser, link)
+        product_page.open()
+        product_page.should_not_be_success_message()
